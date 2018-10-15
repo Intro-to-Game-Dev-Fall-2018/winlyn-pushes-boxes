f178929ff93b14c13ce368248bbf5d1121ef3215
Winlyn Liu <wl1698@nyu.edu>
2018-10-15 03:16:18 -0400

    Added temporary assets, colliders, and movement for the player.

---
+++ Sokoban/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveVertical = Input.GetAxis("Vertical");
+		transform.Translate(0, moveVertical * speed * Time.deltaTime, 0);
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		transform.Translate(moveHorizontal * speed * Time.deltaTime, 0, 0);
+	}
+}

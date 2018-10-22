0f994b518b4c233c68d5b2354c7463adda88366a
Winlyn Liu <wl1698@nyu.edu>
2018-10-22 03:30:06 -0400

    Added crate and brick assets

--- Sokoban/Assets/movement.cs
+++ Sokoban/Assets/movement.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
 
-	public float speed;
+    public float speed;
 	
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
-	}
+    }
 	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		float moveVertical = Input.GetAxis("Vertical");
-		transform.Translate(0, moveVertical * speed * Time.deltaTime, 0);
+    // Update is called once per frame
+    void FixedUpdate () 
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        transform.Translate(0, moveVertical * speed * Time.deltaTime, 0);
 
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		transform.Translate(moveHorizontal * speed * Time.deltaTime, 0, 0);
-	}
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(moveHorizontal * speed * Time.deltaTime, 0, 0);
+    }
 }
